3ffa03ab8c6c63fe6a97f3d78a6f1a23cfcff63a
Angel Melendez <iquit@Angels-MacBook-Pro.local>
2018-08-31 07:12:35 -0400

    pushing to Heroku

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+my_products = [{name: ""}, {}, {}]
+
+
+product = Product.create([
+  {name: "Rainest Boot", description: "Good for rainy days", catagory: "woman", price_in_cents: 1000, image_url: "https://media.hunterboots.com/public/api/cloudfront/image/25181/1200/0.8/90/FFF/0/20170712164456ORG_BLK_AW17_WFT1000RMA_product.jpg"}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  ])
